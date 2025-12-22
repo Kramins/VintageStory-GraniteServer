bf8ff13485d16ed2d84a6cc146a097c2a397fcaf
Chris Kramins <chris@kramins.ca>
2025-12-22 21:16:00 +0000

    Forced Better security defaults.

--- GraniteServerConfig.cs
+++ GraniteServerConfig.cs
@@ -5,10 +5,10 @@ namespace GraniteServer;
 public class GraniteServerConfig
 {
     public int Port { get; set; } = 5000;
-    public string AuthenticationType { get; set; } = "None";
+    public string AuthenticationType { get; set; } = "Basic";
     public string JwtSecret { get; set; } = Guid.NewGuid().ToString();
     public int JwtExpiryMinutes { get; set; } = 60;
     public int JwtRefreshTokenExpiryMinutes { get; set; } = 1440;
     public string? Username { get; set; } = "admin";
-    public string? Password { get; set; } = "password";
+    public string? Password { get; set; } = Guid.NewGuid().ToString();
 }
